981c6d933887bf87c68d59179d15f73fd478100f
tiagocc0 <tiagocc0@gmail.com>
2020-11-13 13:24:08 -0300

    a

---
+++ source/DuplicatedCharactersOnBattleSummaryFix/DuplicatedCharactersOnBattleSummaryFix/DuplicatedCharactersOnBattleSummaryFix.cs
@@ -0,0 +1,56 @@
+﻿
+
+using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+//using System.Xml;
+//using System.IO;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DuplicatedCharactersOnBattleSummaryFix
+{
+
+    [BepInPlugin(GUID, Name, Version)]
+    public class DuplicatedCharactersOnBattleSummaryFix : BaseUnityPlugin
+    {
+        public const string GUID = "net.pog.portalsofphereon.mod.DuplicatedCharactersOnBattleSummaryFix";
+        public const string Name = "Limited Save Mode Reset Count On NGPlus Fix";
+        public const string Version = "1.0";
+
+        private void Start()
+        {
+            UnityEngine.Debug.Log(Name + " loaded (" + GUID + ")");
+
+            var harmony = new Harmony(GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    //Set Halloween flag regardless of date
+    [HarmonyPatch(typeof(InterfaceController))]
+    [HarmonyPatch("showBattleSummary")]
+    static class DuplicatedCharactersOnBattleSummaryFix_Patch
+	{
+		static void Prefix(InterfaceController __instance)
+		{
+            for (int i = GameController.instance.battleCharacterInfos.Count-1; i >=0 ; i--)
+            {
+                BattleCharacterInfo battleCharacterInfo = GameController.instance.battleCharacterInfos[i];
+
+                if (battleCharacterInfo.stats.TownActivity.ToString()=="Reserve")
+                { 
+                    GameController.instance.battleCharacterInfos.RemoveAt(i);
+                }
+            }
+        }
+	}
+}
---
+++ source/LimitedSaveModeResetCountOnNGPlusFix/LimitedSaveModeResetCountOnNGPlusFix/LimitedSaveModeResetCountOnNGPlusFix.cs
@@ -0,0 +1,74 @@
+﻿
+
+using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+//using System.Xml;
+//using System.IO;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace LimitedSaveModeResetCountOnNGPlusFix
+{
+
+    [BepInPlugin(GUID, Name, Version)]
+    public class LimitedSaveModeResetCountOnNGPlusFix : BaseUnityPlugin
+    {
+        public const string GUID = "net.pog.portalsofphereon.mod.LimitedSaveModeResetCountOnNGPlusFix";
+        public const string Name = "Limited Save Mode Reset Count On NGPlus Fix";
+        public const string Version = "1.0";
+
+        private void Start()
+        {
+            UnityEngine.Debug.Log(Name + " loaded (" + GUID + ")");
+
+            var harmony = new Harmony(GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    //Set Halloween flag regardless of date
+    [HarmonyPatch(typeof(SaveController))]
+    [HarmonyPatch("startNewGamePlus")]
+    static class LimitedSaveModeResetCountOnNGPlusFix_Patch
+	{
+		static void Postfix(SaveController __instance)
+		{
+			if (__instance.difficulty.saveType == SaveType.Limited)
+			{
+				switch (__instance.difficulty.gameLength)
+				{
+					case GameLength.QuickRun:
+						__instance.difficulty.loadingLeft = 5;
+						break;
+					case GameLength.VeryShort:
+						__instance.difficulty.loadingLeft = 5;
+						break;
+					case GameLength.Short:
+						__instance.difficulty.loadingLeft = 10;
+						break;
+					case GameLength.Medium:
+						__instance.difficulty.loadingLeft = 15;
+						break;
+					case GameLength.Long:
+						__instance.difficulty.loadingLeft = 20;
+						break;
+					case GameLength.VeryLong:
+						__instance.difficulty.loadingLeft = 25;
+						break;
+					case GameLength.FreePlay:
+						__instance.difficulty.loadingLeft = 50;
+						break;
+				}
+			}
+		}
+	}
+}
